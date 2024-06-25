10056e5652f5d1ad85780a7b04f454d7389d504b
Attila Lászlo Laczkó <laczkoattilalaszlo@gmail.com>
2024-06-25 16:26:27 +0200

    Delete mockserver-client directory

--- mockserver-client/README.md
+++
@@ -1,5 +0,0 @@
-# Client for mockserver-server project
-This is a project to serve as client for the mockserver-server project.
-
-### Build and Run
-1. Run the application
--- mockserver-client/src/main/kotlin/com/laczkoattilalaszlo/mockserverclient/model/Person.kt
+++
@@ -1,8 +0,0 @@
-package com.laczkoattilalaszlo.mockserverclient.model
-
-data class Person(
-    val id: String? = null,
-    val name: String? = null,
-    val age: Int? = null
-    ) {
-}
